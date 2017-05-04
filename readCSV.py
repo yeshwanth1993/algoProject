dea1438b0af7308295eba7e407edd252db66ed60
Sreesha Kashyap <sreeshakashyap@gmail.com>
2017-05-04 18:53:03 -0500

    Add files via upload

--- readCSV.py
+++ readCSV.py
@@ -7,14 +7,15 @@ def read_csv(filename):
     details = [list(map(int, x.split(','))) for x in file_contents ]
     return details
 def generate_inputs_task1(no_of_workers,no_of_tasks):
-    worker_settings,task_settings = [],[]
+    worker_settings,task_settings = {},{}
     for i in range(1,no_of_workers+1):
         max_tasks = random.randint(1,no_of_tasks+1)
-        worker_settings.append([(i,max_tasks)])
+        worker_settings['w'+str(i)] = max_tasks
     for i in range(1,no_of_tasks+1):    
         suitable_workers = random.randint(1, no_of_workers+1, size=random.randint(1,no_of_workers+1))
-        suitable_workers = list(set(suitable_workers))
-        task_settings += [(i,suitable_workers)] 
+        suitable_workers = map(str,list(set(suitable_workers)))
+        suitable_workers = ['w'+s for s in suitable_workers]
+        task_settings['task'+str(i)] = suitable_workers 
     return worker_settings,task_settings    
 
 def generate_inputs_task2(no_of_workers,no_of_tasks):
@@ -39,7 +40,7 @@ def sample_parameters():
         cases += [(t,num_workers,worker_settings,task_settings)] # (num_tasks, num_workers, worker_compatibility)
     return cases
 if __name__ == '__main__':
-    file_contents = read_csv('task_details.csv')
-    print(file_contents)
+    #file_contents = read_csv('task_details.csv')
+    #print(file_contents)
     cases = sample_parameters()
     print(cases[3])

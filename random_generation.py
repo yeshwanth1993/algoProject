78702e90e94d0695b180b85d661f9f0c0a09612b
Yeshwanth D Gunasekaran <yeshwanthgunasekaran@gmail.com>
2017-05-04 22:58:54 -0500

    added all random generation

--- random_generation.py
+++ random_generation.py
@@ -13,6 +13,25 @@ def generate_inputs_task1(no_of_workers, no_of_tasks):
         suitable_workers = ['w'+s for s in suitable_workers]
         task_settings['task'+str(i)] = suitable_workers 
     return task_settings, worker_settings
+def generate_input_task3(no_of_tasks,max_profit=100):
+    tasks = range(1,no_of_tasks+1)
+    dependent_tasks = set(random.choice(tasks,size = random.randint(1,int(2 * no_of_tasks/3.0))))
+    independent_tasks = list(set(tasks) - dependent_tasks)
+    dependent_tasks = list(dependent_tasks)
+    task_config = {}
+    for i in range(0,len(dependent_tasks)):
+        pre_reqs = list(map(str,set(random.choice(independent_tasks,size = len(independent_tasks)+1))))
+        profit = random.randint(1,max_profit+1)
+        task_config[str(dependent_tasks[i])] =[profit , pre_reqs]
+    for i in range(0,len(independent_tasks)):
+        profit = random.randint(1,max_profit+1)
+        task_config[str(independent_tasks[i])] =[-profit , []]
+    return task_config
+def get_input_cases_task3(num_tasks = 1000):
+    graphs = []
+    for num in range(50,num_tasks+1,50):
+         graphs.append((num,generate_input_task3(num)))
+    return graphs
 
 
 def generate_inputs_task2(no_of_workers,no_of_tasks):
